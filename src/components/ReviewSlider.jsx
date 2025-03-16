52ff1f757ac68e11925988fd82c31491b3ea3f8c
akterbd <akter2030@gmail.com>
2025-03-16 10:27:16 +0600

    author image issue fixed

--- src/components/ReviewSlider.jsx
+++ src/components/ReviewSlider.jsx
@@ -1,17 +1,18 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import quate from "@/assets/img/quate.svg";
+import authorPhoto from "@/assets/img/review-author-img.png";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import StarRating from "./StarRating";
 
 const reviews = [
-  { id: 1, name: "John Williams", designation:"Lead designer", rating:4, authorImg:"./src/assets/img/review-author.png", review: "I’ve tried every home remedy and hair care treatment but ended up with long waits and no results. Since I’ve started using Hims my hair has grown, thickened, and darkened tremendously." },
-  { id: 2, name: "John Williams", designation:"Lead designer", rating:4, authorImg:"./src/assets/img/review-author.png", review: "I’ve tried every home remedy and hair care treatment but ended up with long waits and no results. Since I’ve started using Hims my hair has grown, thickened, and darkened tremendously." },
-  { id: 3, name: "John Williams", designation:"Lead designer", rating:4, authorImg:"./src/assets/img/review-author.png", review: "I’ve tried every home remedy and hair care treatment but ended up with long waits and no results. Since I’ve started using Hims my hair has grown, thickened, and darkened tremendously." },
-  { id: 4, name: "John Williams", designation:"Lead designer", rating:4, authorImg:"./src/assets/img/review-author.png", review: "I’ve tried every home remedy and hair care treatment but ended up with long waits and no results. Since I’ve started using Hims my hair has grown, thickened, and darkened tremendously." },
-  { id: 5, name: "John Williams", designation:"Lead designer", rating:4, authorImg:"./src/assets/img/review-author.png", review: "I’ve tried every home remedy and hair care treatment but ended up with long waits and no results. Since I’ve started using Hims my hair has grown, thickened, and darkened tremendously." },
+  { id: 1, name: "John Williams", designation:"Lead designer", rating:4, authorImg:authorPhoto, review: "I’ve tried every home remedy and hair care treatment but ended up with long waits and no results. Since I’ve started using Hims my hair has grown, thickened, and darkened tremendously." },
+  { id: 2, name: "John Williams", designation:"Lead designer", rating:4, authorImg:authorPhoto, review: "I’ve tried every home remedy and hair care treatment but ended up with long waits and no results. Since I’ve started using Hims my hair has grown, thickened, and darkened tremendously." },
+  { id: 3, name: "John Williams", designation:"Lead designer", rating:4, authorImg:authorPhoto, review: "I’ve tried every home remedy and hair care treatment but ended up with long waits and no results. Since I’ve started using Hims my hair has grown, thickened, and darkened tremendously." },
+  { id: 4, name: "John Williams", designation:"Lead designer", rating:4, authorImg:authorPhoto, review: "I’ve tried every home remedy and hair care treatment but ended up with long waits and no results. Since I’ve started using Hims my hair has grown, thickened, and darkened tremendously." },
+  { id: 5, name: "John Williams", designation:"Lead designer", rating:4, authorImg:authorPhoto, review: "I’ve tried every home remedy and hair care treatment but ended up with long waits and no results. Since I’ve started using Hims my hair has grown, thickened, and darkened tremendously." },
 ];
 
 const ReviewSlider = () => {
